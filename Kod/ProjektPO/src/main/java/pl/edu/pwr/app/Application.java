ea18521892ce7f41180cd8631bc46cc676a85e58
AleksandraPiatek <104870546+AleksandraPiatek@users.noreply.github.com>
2022-05-17 23:08:58 +0200

    argumnetsParser added

--- Kod/ProjektPO/src/main/java/pl/edu/pwr/app/Application.java
+++ Kod/ProjektPO/src/main/java/pl/edu/pwr/app/Application.java
@@ -4,7 +4,9 @@ import pl.edu.pwr.simulation.Simulation;
 
 public class Application extends ArgumentParser {
     public static void main(String[] args){
+        ArgumentParser argumentsParser= new ArgumentParser();
         Simulation simulation = new Simulation(50, 10);
         System.out.println(simulation.simulate());
+
     }
 }
