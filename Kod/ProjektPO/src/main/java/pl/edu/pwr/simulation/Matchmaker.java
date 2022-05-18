c9a0a6677c3adf3e39b5d032a524545de88008b2
MaciejPadula <50674232+MaciejPadula@users.noreply.github.com>
2022-05-18 17:52:19 +0200

    Matchmaker
    Added basic matching

--- Kod/ProjektPO/src/main/java/pl/edu/pwr/simulation/Matchmaker.java
+++ Kod/ProjektPO/src/main/java/pl/edu/pwr/simulation/Matchmaker.java
@@ -2,6 +2,7 @@ package pl.edu.pwr.simulation;
 
 public class Matchmaker {
     public void match(Person person1, Person person2){
-
+        person1.setPartner(person2);
+        person2.setPartner(person1);
     }
 }
