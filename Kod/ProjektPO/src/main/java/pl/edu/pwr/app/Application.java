6fc41133cba7b888f8edd31d0084daed05e554c4
MaciejPadula <50674232+MaciejPadula@users.noreply.github.com>
2022-05-31 13:09:41 +0200

    jar file created

--- Kod/ProjektPO/src/main/java/pl/edu/pwr/app/Application.java
+++ Kod/ProjektPO/src/main/java/pl/edu/pwr/app/Application.java
@@ -1,13 +1,21 @@
 package pl.edu.pwr.app;
 
+import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import pl.edu.pwr.app.arguments.ApplicationArguments;
 import pl.edu.pwr.app.arguments.ArgumentParser;
 import pl.edu.pwr.simulation.Simulation;
 
+import java.io.File;
+
 public class Application extends ArgumentParser {
     public static void main(String[] args){
-        ApplicationArguments applicationArguments = new ApplicationArguments(ConfigFactory.load());
+        Config baseConfig = ConfigFactory.load();
+        Config config = ConfigFactory
+                .parseFile(new File("application.json"))
+                .withFallback(baseConfig);
+
+        ApplicationArguments applicationArguments = new ApplicationArguments(config);
         Simulation simulation = new Simulation(applicationArguments);
         System.out.println(simulation.simulate());
     }
